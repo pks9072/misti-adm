7a4aae6b6db9779bd7b29ccce573ac4f7f074a88
kspark <pks9072@gmail.com>
2022-05-08 23:58:43 +0900

    Add 제휴, 브랜드

---
+++ database/migrations/2022_05_07_011115_create_affiliates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('affiliates', function (Blueprint $table) {
+            $table->id();
+            $table->string('aname');
+            $table->decimal('fees', 5,2)->default(000.00);
+            $table->integer('state')->default(50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('affiliates');
+    }
+};
