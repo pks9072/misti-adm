43865a3d5f0f08e5eab5c0da4c79655fb4a2d481
kspark <kspark@ebiznetworks.co.kr>
2022-05-09 11:13:47 +0900

    Fix 로그인

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,16 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
+    //return view('dashboard');
+    return redirect()->route('rent.index');
+})->name('home');
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
