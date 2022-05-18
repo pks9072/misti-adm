8a8b1608ee060a85802787c0c52b8a5033da76d3
kspark <kspark@ebiznetworks.co.kr>
2022-05-18 17:03:18 +0900

    Fix 타이틀

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>살구마켓 관리자</title>
+    <title>{{ config("app.name") }}</title>
     <link rel="stylesheet" href="{{ asset('common/css/reset.css') }}" />
     <link rel="stylesheet" href="{{ asset('common/css/common.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/login.css') }}" />
