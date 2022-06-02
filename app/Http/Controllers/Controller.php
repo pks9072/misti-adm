3ae3d4efaebdf488536bdf71c7b8b88649feda4d
kspark <kspark@ebiznetworks.co.kr>
2022-06-02 15:37:41 +0900

    Fix 이블레 파싱 테스트

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,12 +10,4 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function index() {
-        $path = "http://y-ble.com/kspark/product-list.php";
-        $test = json_decode(file_get_contents($path), true);
-
-        echo "<pre>";
-        print_r($test);
-    }
 }
