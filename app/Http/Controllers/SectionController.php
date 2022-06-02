2244b827ec9d3fc39f2dd15e2615d066ff8ddd94
kspark <kspark@ebiznetworks.co.kr>
2022-06-02 15:38:58 +0900

    Fix 이블레 대분류 구분

--- app/Http/Controllers/SectionController.php
+++ app/Http/Controllers/SectionController.php
@@ -98,4 +98,19 @@ class SectionController extends Controller
     {
         //
     }
+
+    public function yble($data) {
+        $section = match($data) {
+            "1001"  => 1,       // WOMAN
+            "1002"  => 2,       // MEN
+            "1309"  => 3,       // GOLF
+            "1438"  => 4,       // LUXURY
+            "1308"  => 5,       // JEWELRY
+            "1307"  => 6,       // COSMETICS
+            "1306"  => 7,       // LIFE
+            "1458"  => 8,       // PET
+        };
+
+        return $section;
+    }
 }
