1c1aec0cb8254339bc5095582199a4353a0b9a6c
kspark <kspark@ebiznetworks.co.kr>
2022-06-03 11:04:36 +0900

    Fix 이블레 모델로 이동

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -13,4 +13,19 @@ class Section extends Model
         $list = Section::where()->orderBy("sort", "asc");
         return $list;
     }
+
+    public function yble($data) {
+        $section = match($data) {
+            "1001"  => 1,       // WOMAN
+            "1002"  => 2,       // MEN
+            "1309"  => 3,       // GOLF
+            "1438"  => 4,       // LUXURY
+            "1308"  => 5,       // JEWELRY
+            "1307"  => 6,       // COSMETICS
+            "1306"  => 7,       // LIFE
+            "1458"  => 8,       // PET
+        };
+
+        return $section;
+    }
 }
